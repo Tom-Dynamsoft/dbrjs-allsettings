130862da87e29358946e560c0180d8319c70530e
tom@dynamsoft.com <tom@dynamsoft.com>
2021-11-01 15:02:18 +0800

    keep it in github

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SettingsSample from './components/SettingsSample';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SettingsSample title="Barcode Reader Settings Sample"></SettingsSample>
+    </div>
+  );
+}
+
+export default App;
